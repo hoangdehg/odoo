32cdac27cf8355af98154c9ebeed2beaf2295667
Nam <phamhoainamktak59@gmail.com>
2026-01-27 09:56:45 +0700

    khoi tao module subcontract

---
+++ addons/estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'Manage real estate properties',
+    'description': 'A minimal module for estate management',
+    'category': 'Real Estate',
+    'author': 'Your Name',
+    'depends': ['base'],
+    'data': ['security/ir.model.access.csv', 
+             'views/estate_property_views.xml',
+             'views/estate_menus.xml',],    
+    'installable': True,
+    'application': True,
+}
---
+++ addons/subcontract_management/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Subcontract Management',
+    'version': '1.0',
+    'summary': 'Manage subcontract orders and materials',
+    'description': 'A minimal module for subcontract management',
+    'category': 'Subcontract Management',
+    'author': 'AZ',
+    'depends': ['base'],
+        
+    'installable': True,
+    'application': True,
+}
