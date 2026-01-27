c14f396a6cc8674071a04e708a32971db85310fc
Nam <phamhoainamktak59@gmail.com>
2026-01-27 15:45:39 +0700

    them user id model order

--- addons/subcontract_management/models/subcontract_order.py
+++ addons/subcontract_management/models/subcontract_order.py
@@ -12,5 +12,6 @@ class SubcontractOrder(models.Model):
                               ('cancelled', 'Hủy'),
                               ('done', 'Hoàn thành')], string="Trạng Thái", default='draft')
     partner_id = fields.Many2one('res.partner', string="Đơn vị gia công")
+    user_id = fields.Many2one('res.users', string="Người tạo", default=lambda self: self.env.user)
     '''order_date = fields.Date(string="Order Date", default=fields.Date.context_today)
     material_ids = fields.One2many('subcontract.material', 'order_id', string="Materials")'''
