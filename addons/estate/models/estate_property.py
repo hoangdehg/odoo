81b33db9a03a1b40bb4386b7c5ec510db9e97601
Nam <phamhoainamktak59@gmail.com>
2026-02-04 10:00:43 +0700

    them offer chương 7

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -31,6 +31,7 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string="Tags")
+    offer_ids = fields.One2many('estate.property.offer', 'property_id', string="Offers")
     active = fields.Boolean(default=True)
     state = fields.Selection(selection=[
         ('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'),
